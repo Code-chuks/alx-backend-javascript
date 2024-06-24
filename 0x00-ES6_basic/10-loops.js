e4b0200c753094018d5f837bae06fc0d842024a1
Code-chuks <ezeuzorchukwudi@gmail.com>
2024-06-24 21:18:11 +0000

    js is cool

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString)
+{
+  const returnArray = [];
+  for (const idx of array) {
+    returnArray.push(appendString + idx);
+  }
+
+  return returnArray;
+}
